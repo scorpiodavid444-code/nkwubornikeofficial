7017d78f6d520461caa6fda5cbcfcac7c4db16e2
precious <ezechukwuifeoma279@gmail.com>
2026-03-14 23:21:41 +0100

    new

--- public/script.js
+++ public/script.js
@@ -80,6 +80,38 @@ document.querySelectorAll(".dropdown-toggle").forEach(toggle => {
     }, 6000);
 
 
+    let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(i){
+
+slides.forEach(slide => slide.classList.remove("active"));
+
+slides[i].classList.add("active");
+
+}
+
+document.querySelector(".next").onclick = () => {
+
+index++;
+
+if(index >= slides.length) index = 0;
+
+showSlide(index);
+
+}
+
+document.querySelector(".prev").onclick = () => {
+
+index--;
+
+if(index < 0) index = slides.length - 1;
+
+showSlide(index);
+
+}
+
+
 
 const NEWS_SOURCE = "/news.json";
 
